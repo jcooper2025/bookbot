910b91b4b3a907d9dca0f82ae421cb7ca10c82c5
jcooper2025 <jonatcooper@gmail.com>
2025-02-19 23:58:57 -0600

    final version of main for project bookbot

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 
+# def sort_on(dict):
+#     return dict[]
+
+def character_count(book) -> dict:
+    working_book = book.lower()
+
+    char_inventory = {}
+    for c in working_book:
+        if c not in char_inventory.keys():
+            char_inventory[c] = 1
+        else:
+            char_inventory[c] += 1
+
+    return char_inventory
+
+
+
 def count_words(book):
     return len(book.split())
 
 def main():
 
+    list_of_char_inv = []
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(count_words(file_contents))
+    words = count_words(file_contents)
+    print(f"{words} words found in the document\n")
+
+    count_characters = character_count(file_contents)
+
+    for key, item in count_characters.items():
+        if key.isalpha():
+            list_of_char_inv.append({key: item})
+
+    list_of_char_inv.sort(key=lambda d: list(d.values())[0], reverse=True)
+
+    for ele in list_of_char_inv:
+        key, item = next(iter(ele.items()))
+        print(f"The '{key}' character is found {item} times")
 
 
 if __name__ == "__main__":
